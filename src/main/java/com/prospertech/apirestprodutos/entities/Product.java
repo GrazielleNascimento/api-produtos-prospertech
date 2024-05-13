fb4779958cdc6a6cc0931385829ac9a13cda7a0c
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-05-12 21:36:44 -0300

    fix:  correcting entity price type

--- src/main/java/com/prospertech/apirestprodutos/entities/Product.java
+++ src/main/java/com/prospertech/apirestprodutos/entities/Product.java
@@ -2,12 +2,13 @@ package com.prospertech.apirestprodutos.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
+
 
 @Getter
 @Setter
@@ -23,9 +24,9 @@ public class Product {
     @NotBlank(message = "Name is requerid!")
     private String name;
 
-    @NotBlank(message = "Price is required!")
+    @NotNull(message = "Price is required!")
     @Positive(message = "The price must be greater than zero!")
-    private BigDecimal price;
+    private Double price;
 
     @Column(length = 500)
     @Size(max=500)
